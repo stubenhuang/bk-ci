a63b2654b00a7d56065f3fa1adf14d62d0f8ed53
stubenhuang <stubenhuang@tencent.com>
2025-11-17 17:42:39 +0800

    feat: 蓝盾支持多租户模式 #11524 修复BUG

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/open/OpenPipelineTaskResource.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/open/OpenPipelineTaskResource.kt
@@ -28,7 +28,11 @@
 package com.tencent.devops.process.api.open
 
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_BK_TOKEN
+import com.tencent.devops.common.api.pojo.BuildHistoryPage
 import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.pipeline.enums.BuildStatus
+import com.tencent.devops.common.pipeline.enums.StartType
+import com.tencent.devops.process.pojo.BuildHistory
 import com.tencent.devops.process.pojo.Pipeline
 import com.tencent.devops.process.pojo.classify.PipelineViewPipelinePage
 import com.tencent.devops.process.pojo.open.BuildStatusInfo
@@ -90,4 +94,100 @@ interface OpenPipelineTaskResource {
         @QueryParam("pageSize")
         pageSize: Int? = 20
     ): Result<PipelineViewPipelinePage<Pipeline>>
+
+    @Operation(summary = "获取流水线构建历史-new")
+    @GET
+    @Path("/build/history/new")
+    fun getHistoryBuildNew(
+        @Parameter(description = "认证token", required = true)
+        @HeaderParam(AUTH_HEADER_DEVOPS_BK_TOKEN)
+        token: String,
+        @Parameter(description = "用户ID", required = true)
+        @QueryParam("userId")
+        userId: String,
+        @Parameter(description = "项目ID", required = true)
+        @QueryParam("projectId")
+        projectId: String,
+        @Parameter(description = "流水线ID", required = true)
+        @QueryParam("pipelineId")
+        pipelineId: String,
+        @Parameter(description = "第几页", required = false)
+        @QueryParam("page")
+        page: Int?,
+        @Parameter(description = "每页多少条", required = false)
+        @QueryParam("pageSize")
+        pageSize: Int?,
+        @Parameter(description = "源材料代码库别名", required = false)
+        @QueryParam("materialAlias")
+        materialAlias: List<String>?,
+        @Parameter(description = "代码库URL", required = false)
+        @QueryParam("materialUrl")
+        materialUrl: String?,
+        @Parameter(description = "源材料分支", required = false)
+        @QueryParam("materialBranch")
+        materialBranch: List<String>?,
+        @Parameter(description = "commitId", required = false)
+        @QueryParam("materialCommitId")
+        materialCommitId: String?,
+        @Parameter(description = "commitMessage", required = false)
+        @QueryParam("materialCommitMessage")
+        materialCommitMessage: String?,
+        @Parameter(description = "状态", required = false)
+        @QueryParam("status")
+        status: List<BuildStatus>?,
+        @Parameter(description = "触发方式", required = false)
+        @QueryParam("trigger")
+        trigger: List<StartType>?,
+        @Parameter(description = "排队于-开始时间(时间戳形式)", required = false)
+        @QueryParam("queueTimeStartTime")
+        queueTimeStartTime: Long?,
+        @Parameter(description = "排队于-结束时间(时间戳形式)", required = false)
+        @QueryParam("queueTimeEndTime")
+        queueTimeEndTime: Long?,
+        @Parameter(description = "开始于-开始时间(时间戳形式)", required = false)
+        @QueryParam("startTimeStartTime")
+        startTimeStartTime: Long?,
+        @Parameter(description = "开始于-结束时间(时间戳形式)", required = false)
+        @QueryParam("startTimeEndTime")
+        startTimeEndTime: Long?,
+        @Parameter(description = "结束于-开始时间(时间戳形式)", required = false)
+        @QueryParam("endTimeStartTime")
+        endTimeStartTime: Long?,
+        @Parameter(description = "结束于-结束时间(时间戳形式)", required = false)
+        @QueryParam("endTimeEndTime")
+        endTimeEndTime: Long?,
+        @Parameter(description = "耗时最小值", required = false)
+        @QueryParam("totalTimeMin")
+        totalTimeMin: Long?,
+        @Parameter(description = "耗时最大值", required = false)
+        @QueryParam("totalTimeMax")
+        totalTimeMax: Long?,
+        @Parameter(description = "备注", required = false)
+        @QueryParam("remark")
+        remark: String?,
+        @Parameter(description = "构件号起始", required = false)
+        @QueryParam("buildNoStart")
+        buildNoStart: Int?,
+        @Parameter(description = "构件号结束", required = false)
+        @QueryParam("buildNoEnd")
+        buildNoEnd: Int?,
+        @Parameter(description = "构建信息", required = false)
+        @QueryParam("buildMsg")
+        buildMsg: String?,
+        @Parameter(description = "是否查询归档数据", required = false)
+        @QueryParam("archiveFlag")
+        archiveFlag: Boolean? = false,
+        @Parameter(description = "指定调试数据", required = false)
+        @QueryParam("debug")
+        debug: Boolean? = null,
+        @Parameter(description = "触发代码库", required = false)
+        @QueryParam("triggerAlias")
+        triggerAlias: List<String>?,
+        @Parameter(description = "触发分支", required = false)
+        @QueryParam("triggerBranch")
+        triggerBranch: List<String>?,
+        @Parameter(description = "触发方式", required = false)
+        @QueryParam("triggerUser")
+        triggerUser: List<String>?
+    ): Result<BuildHistoryPage<BuildHistory>>
 }
--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/api/open/OpenPipelineTaskResourceImpl.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/api/open/OpenPipelineTaskResourceImpl.kt
@@ -1,24 +1,30 @@
 package com.tencent.devops.process.api.open
 
+import com.tencent.devops.common.api.pojo.BuildHistoryPage
 import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.pipeline.enums.BuildStatus
 import com.tencent.devops.common.pipeline.enums.ChannelCode
+import com.tencent.devops.common.pipeline.enums.StartType
 import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.common.web.annotation.BkApiPermission
 import com.tencent.devops.common.web.constant.BkApiHandleType
 import com.tencent.devops.process.engine.service.PipelineRuntimeService
 import com.tencent.devops.process.engine.service.PipelineTaskService
+import com.tencent.devops.process.pojo.BuildHistory
 import com.tencent.devops.process.pojo.Pipeline
 import com.tencent.devops.process.pojo.PipelineSortType
 import com.tencent.devops.process.pojo.classify.PipelineViewPipelinePage
 import com.tencent.devops.process.pojo.open.BuildStatusInfo
 import com.tencent.devops.process.service.PipelineListFacadeService
+import com.tencent.devops.process.service.builds.PipelineBuildFacadeService
 import org.springframework.beans.factory.annotation.Autowired
 
 @RestResource
 class OpenPipelineTaskResourceImpl @Autowired constructor(
     private val pipelineTaskService: PipelineTaskService,
     private val pipelineRuntimeService: PipelineRuntimeService,
-    private val pipelineListFacadeService: PipelineListFacadeService
+    private val pipelineListFacadeService: PipelineListFacadeService,
+    private val pipelineBuildFacadeService: PipelineBuildFacadeService
 ) : OpenPipelineTaskResource {
 
     override fun getBuildStatus(
@@ -66,6 +72,75 @@ class OpenPipelineTaskResourceImpl @Autowired constructor(
             sortType = PipelineSortType.CREATE_TIME,
             channelCode = ChannelCode.BS,
             viewId = viewId,
+            checkPermission = false
+        )
+        return Result(result)
+    }
+
+    @Suppress("LongParameterList", "UNUSED_PARAMETER")
+    @BkApiPermission([BkApiHandleType.API_OPEN_TOKEN_CHECK])
+    override fun getHistoryBuildNew(
+        token: String,
+        userId: String,
+        projectId: String,
+        pipelineId: String,
+        page: Int?,
+        pageSize: Int?,
+        materialAlias: List<String>?,
+        materialUrl: String?,
+        materialBranch: List<String>?,
+        materialCommitId: String?,
+        materialCommitMessage: String?,
+        status: List<BuildStatus>?,
+        trigger: List<StartType>?,
+        queueTimeStartTime: Long?,
+        queueTimeEndTime: Long?,
+        startTimeStartTime: Long?,
+        startTimeEndTime: Long?,
+        endTimeStartTime: Long?,
+        endTimeEndTime: Long?,
+        totalTimeMin: Long?,
+        totalTimeMax: Long?,
+        remark: String?,
+        buildNoStart: Int?,
+        buildNoEnd: Int?,
+        buildMsg: String?,
+        archiveFlag: Boolean?,
+        debug: Boolean?,
+        triggerAlias: List<String>?,
+        triggerBranch: List<String>?,
+        triggerUser: List<String>?
+    ): Result<BuildHistoryPage<BuildHistory>> {
+        val result = pipelineBuildFacadeService.getHistoryBuild(
+            userId = userId,
+            projectId = projectId,
+            pipelineId = pipelineId,
+            page = page,
+            pageSize = pageSize,
+            materialAlias = materialAlias,
+            materialUrl = materialUrl,
+            materialBranch = materialBranch,
+            materialCommitId = materialCommitId,
+            materialCommitMessage = materialCommitMessage,
+            status = status,
+            trigger = trigger,
+            queueTimeStartTime = queueTimeStartTime,
+            queueTimeEndTime = queueTimeEndTime,
+            startTimeStartTime = startTimeStartTime,
+            startTimeEndTime = startTimeEndTime,
+            endTimeStartTime = endTimeStartTime,
+            endTimeEndTime = endTimeEndTime,
+            totalTimeMin = totalTimeMin,
+            totalTimeMax = totalTimeMax,
+            remark = remark,
+            buildNoStart = buildNoStart,
+            buildNoEnd = buildNoEnd,
+            buildMsg = buildMsg,
+            archiveFlag = archiveFlag,
+            debug = debug,
+            triggerAlias = triggerAlias,
+            triggerBranch = triggerBranch,
+            triggerUser = triggerUser,
             checkPermission = false
         )
         return Result(result)
